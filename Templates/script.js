b7fc7c8a4a641ebc69d25d03a79f8a4c7f974efd
Luke Peterson <Lukep.Creative@gmail.com>
2026-05-28 15:07:01 -0500

    Added timestamps and saved data to local storage in the weather tracker application.

--- Templates/script.js
+++ Templates/script.js
@@ -15,9 +15,60 @@ document.getElementById("submit-button").addEventListener("click", function() {
     let weatherLog =
         document.getElementById("weather-log");
 
-    weatherLog.innerHTML += `
+        if (temperature == "" || humidity == "") {
+
+        document.getElementById("data-recorded")
+        .style.display = "none";  
+    
+
+    weatherLog.innerHTML = `
+        <p>Please enter both temperature and humidity.</p>
+    </p>
+    `;
+
+    return;
+}
+
+     let currentDate = new Date();
+        let formattedDate = currentDate.toLocaleString();
+        
+    let weatherData = {
+        temperature: temperature,
+        humidity: humidity,
+        date: formattedDate
+    };
+
+    localStorage.setItem("weatherData", JSON.stringify(weatherData));
+
+    weatherLog.innerHTML = `
         <p>${temperature}&deg;F | ${humidity}%</p>
+        <p>
+        Recorded: ${formattedDate}
+    </p>
     `;
 document.getElementById("Temperature").value = "";
 document.getElementById("Humidity").value = "";
 });
+
+let savedWeather =
+    localStorage.getItem("weatherData");
+
+if (savedWeather) {
+
+    savedWeather =
+        JSON.parse(savedWeather);
+
+    document.getElementById("weather-log")
+        .innerHTML = `
+        <p class="data-recorded">
+            ${savedWeather.temperature}&deg;F |
+            ${savedWeather.humidity}%
+        </p>
+
+        <p>
+            Recorded:
+            ${savedWeather.date}
+        </p>
+    `;
+
+}
